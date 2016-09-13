14d9eeae1c7cab4a670994912ac0160d4ea83c01
Ben Demko <bendemko7@gmail.com>
2016-09-13 17:58:43 -0400

    Add project files.

---
+++ Fortune-teller-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fortune_teller_mvc.Startup))]
+namespace Fortune_teller_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
